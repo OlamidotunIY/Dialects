910eda32c3d494fd18ae433cf8ee44ee7209482f
Iyanda Dotun <olamidotun225@gmail.com>
2023-12-07 12:52:08 +0100

    stripe updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 import stripe from "../utills/stripe.js";
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
 export const register = async (req, res) => {
@@ -14,12 +13,15 @@ export const register = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const customer = await stripe.customers.create({
-      email: email,
-      name: fullName,
-    }, {
-      apiKey: process.env.STRIPE_SECRET_KEY,
-    })
+    const customer = await stripe.customers.create(
+      {
+        email: email,
+        name: fullName,
+      },
+      {
+        apiKey: process.env.STRIPE_SECRET_KEY,
+      }
+    );
 
     const newUser = new User({
       fullName: fullName,
@@ -42,15 +44,16 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email: email });
-    const subscription = await stripe.subscriptions.list({
-      customer: user.customerID,
-      status: 'all',
-      expand: ['data.default_payment_method'],
-    },
+    const subscription = await stripe.subscriptions.list(
+      {
+        customer: user.customerID,
+        status: "all",
+        expand: ["data.default_payment_method"],
+      },
       {
         apiKey: process.env.STRIPE_SECRET_KEY,
       }
-    )
+    );
     if (!user) {
       return res.status(400).json({ message: "User does not exist" });
     }
@@ -61,12 +64,20 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Incorrect Password" });
     }
 
-    let plan = ""
-    if(!subscription.data.length){
-      plan = "free"
+    let plan = null;
+    if (!subscription.data.length) {
+      plan = "free";
     }
 
-    plan = subscription.data[0].plan.nickname
+    if (subscription.data.length) {
+      if (subscription.data[0].plan.nickname == "Basic") {
+        plan = "basic";
+      } else if (subscription.data[0].plan.nickname == "Pro") {
+        plan = "pro";
+      } else if (subscription.data[0].plan.nickname == "Master") {
+        plan = "master";
+      }
+    }
     const token = Jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ user, token, plan });
